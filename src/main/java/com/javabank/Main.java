def24cbf47634cded004a3235befa700b6146bfb
blackbird17922se <blackbird17922@gmail.com>
2024-10-12 19:51:04 -0500

    Inicio PY

---
+++ src/main/java/com/javabank/Main.java
@@ -0,0 +1,7 @@
+package com.javabank;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/javabank/Xe.java
@@ -0,0 +1,7 @@
+package com.javabank;
+
+public class Xe {
+    public static void main(String[] args) {
+        System.out.println("Mauuuuuuuus");
+    }
+}
